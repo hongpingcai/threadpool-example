2f6998e5ff4fe332fc727db56d55da504ff6311b
Hongping Cai <hongping.cai.80@gmail.com>
2018-09-17 10:54:13 +0100

    Fixed a few bugs and added doc comments to Job class

--- Job.java
+++ Job.java
@@ -1,16 +1,23 @@
-public class Job impleents Runnable {
-    private int jobNumber;
-    Job (int jobNumber){
+/**
+ * The Job class which implements Runnable interface
+ * 
+ */
+public class Job implements Runnable {
+    
+    private int jobNumber;//attribute job ID
+    public Job (int jobNumber){
         this.jobNumber = jobNumber;
     }
-    
+    /**
+     * 
+     */
     public void run() {
         //undertake required work, her we will emulate it by sleeping for a period
         System.out.println("Job: " + jobNumber + " is being processed by thread: "
           + Thread.currentThread().getName());
         try {
             Thread.sleep((int)(1000));
-        } catch (InterruptedExeption e) {
+        } catch (InterruptedException e) {
             //no catching as example shoule not experince interruptions
         }
         System.out.println("Job: " + jobNumber + " is ending in Thread "
